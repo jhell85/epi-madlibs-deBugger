e967fc0703a91cf9b535dd00167f4020c6242321
josh <jhell85@gmail.com>
2020-03-25 09:10:50 -0700

    created project and added some js and html

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+  $("#blanks form").submit(function() {
+    alert('Got to beginning of form submit!');
+    var blanks = ["person1", "person2", "animal", "exclamation", "verb", "noun"];
+
+    blanks.forEach(function(blank) {
+      debugger;
+      var userInput = $("input." + blank).val();
+      $("." + blank).text(userInput).val();      
+    });
+
+    $("#story").show();
+    event.preventDefault();
+  });
+});
